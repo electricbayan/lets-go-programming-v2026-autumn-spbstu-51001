eba9b6c84d46cd4bedce88dfe6e6ff2920433304
artem <a.volkovich@internet.ru>
2026-09-13 15:43:25 +0300

    [TASK-1] Formatting

--- artem.volkovich/task-1/cmd/service/main.go
+++ artem.volkovich/task-1/cmd/service/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	var secondArg int
 	_, err = fmt.Scan(&secondArg)
-	
+
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
@@ -22,7 +22,7 @@ func main() {
 
 	var operator string
 	_, err = fmt.Scan(&operator)
-	
+
 	switch operator {
 	case "+":
 		fmt.Println(firstArg + secondArg)
@@ -31,7 +31,7 @@ func main() {
 	case "*":
 		fmt.Println(firstArg * secondArg)
 	case "/":
-		if (secondArg != 0) {
+		if secondArg != 0 {
 			fmt.Println(firstArg / secondArg)
 		} else {
 			fmt.Println("Division by zero")
