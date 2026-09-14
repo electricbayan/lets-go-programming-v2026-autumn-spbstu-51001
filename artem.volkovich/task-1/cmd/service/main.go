cecaa40d71316d6f87ec59ee9d2c2fd011814018
artem <a.volkovich@internet.ru>
2026-09-14 17:49:20 +0300

    [TASK-1] Removing internal

--- artem.volkovich/task-1/cmd/service/main.go
+++ artem.volkovich/task-1/cmd/service/main.go
@@ -1,9 +1,40 @@
 package main
 
-import (
-	"github.com/electricbayan/task-1/internal/calc"
-)
+import "fmt"
 
 func main() {
-	calc.Calc()
+	var firstArg, secondArg int
+	_, err := fmt.Scan(&firstArg)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&secondArg)
+
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var operator string
+	_, _ = fmt.Scan(&operator)
+
+	switch operator {
+	case "+":
+		fmt.Println(firstArg + secondArg)
+	case "-":
+		fmt.Println(firstArg - secondArg)
+	case "*":
+		fmt.Println(firstArg * secondArg)
+	case "/":
+		if secondArg != 0 {
+			fmt.Println(firstArg / secondArg)
+		} else {
+			fmt.Println("Division by zero")
+			return
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+
 }
